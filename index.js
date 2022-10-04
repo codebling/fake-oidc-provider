c41b3c62ac3dc482d4e6e0a604a749b71dba7a75
Codebling <codeblingx@gmail.com>
2022-10-04 16:06:54 -0400

    Add basic implementation

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+const express = require('express');
 const { Provider } = require('oidc-provider');
+
+const app = express();
+
 const oidc = new Provider('http://localhost:3000', {
   features: {
     registration: {
       enabled: true,
     },
+    devInteractions: { enabled: false },
   },
 });
 
-oidc.listen(3000);
+
+app.use('/interaction', async (req, res, next) => {
+  const details = await oidc.interactionDetails(req, res);
+  return oidc.interactionFinished(req, res, {
+    login: {
+      accountId: 'abc',
+    },
+  });
+});
+app.use(oidc.callback());
+app.listen(3000);
+// oidc.use(app);
+// oidc.listen(3000);
