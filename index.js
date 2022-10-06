51188dc25645e9c639660bb69cf096e14a0a6406
Codebling <codeblingx@gmail.com>
2022-10-06 12:44:14 -0400

    Add option to force failure response via environment variable

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { Provider } = require('oidc-provider');
+const { Provider, errors: { InvalidGrant }, } = require('oidc-provider');
 const { nanoid } = require('nanoid');
 
 const app = express();
 
 const {
   PORT = 3000,
+  FAIL = false,
 } = process.env;
 
 const oidc = new Provider(`http://localhost:${PORT}`, {
@@ -19,40 +20,44 @@ const oidc = new Provider(`http://localhost:${PORT}`, {
 const { Grant } = oidc;
 
 app.use('/interaction', async (req, res, next) => {
-  const accountId = nanoid();
-  const { 
-    grantId,
-    params: { client_id: clientId }, 
-    prompt: { 
-      details: {
-        missingOIDCScope = [],
-        missingOIDCClaims = [],
-        missingResourceScopes = {},
-      },     
-    },
-  } = await oidc.interactionDetails(req, res);
+  if (FAIL) {
+    return oidc.interactionFinished(req, res, new InvalidGrant());
+  } else {
+    const accountId = nanoid();
+    const { 
+      grantId,
+      params: { client_id: clientId }, 
+      prompt: { 
+        details: {
+          missingOIDCScope = [],
+          missingOIDCClaims = [],
+          missingResourceScopes = {},
+        },     
+      },
+    } = await oidc.interactionDetails(req, res);
 
-  const grant = grantId != null
-    ? await Grant.find(grantId)
-    : new Grant({
+    const grant = grantId != null
+      ? await Grant.find(grantId)
+      : new Grant({
+          accountId,
+          clientId,
+        });
+
+    grant.addOIDCScope(missingOIDCScope.join(' '));
+    grant.addOIDCClaims(missingOIDCClaims);
+    Object.entries(missingResourceScopes).forEach( ([indicator, scopes]) => grant.addResourceScope(indicator, scopes.join(' ')));
+
+    const newGrantId = await grant.save();
+
+    return oidc.interactionFinished(req, res, {
+      login: {
         accountId,
-        clientId,
-      });
-
-  grant.addOIDCScope(missingOIDCScope.join(' '));
-  grant.addOIDCClaims(missingOIDCClaims);
-  Object.entries(missingResourceScopes).forEach( ([indicator, scopes]) => grant.addResourceScope(indicator, scopes.join(' ')));
-
-  const newGrantId = await grant.save();
-
-  return oidc.interactionFinished(req, res, {
-    login: {
-      accountId,
-    },
-    consent: { 
-      grantId: newGrantId
-    },
-  });
+      },
+      consent: { 
+        grantId: newGrantId
+      },
+    });
+  }
 });
 app.use(oidc.callback());
 app.listen(PORT);
